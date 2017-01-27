62134f335760c4965647395a902608e494097f5f
nmred <nmred_2008@126.com>
2017-01-27 12:04:30 +0800

    modify

--- shell/Mysql.php
+++ shell/Mysql.php
@@ -4,13 +4,18 @@ class GetDb{
 		private static $instance = null;
 		private function __construct(){
 			$database_info = parse_ini_file('./shell.ini');
-			$database = $database_info['dbDatabase'];
-			$hostname = $database_info['dbHostname'];
-			$password = $database_info['dbPassword'];
-			$username = $database_info['dbUsername'];
-			$port = $database_info['dbPort'];
-			$dsn = 'mysql:dbname='.$database.';host='.$hostname.';port='.$port;
-			$this->db = new \PDO($dsn, $username, $password);
+			// nmred
+			// 使用 sprintf 拼接多个变量到字符串中
+			$dsn = sprintf('mysql:dbname=%s;host=%s;port=%s', $database_info['dbDatabase'],
+				$database_info['dbHostname'], $database_info['dbPort']);
+			//$database = $database_info['dbDatabase'];
+			//$hostname = $database_info['dbHostname'];
+			//$password = $database_info['dbPassword'];
+			//$username = $database_info['dbUsername'];
+			//$port = $database_info['dbPort'];
+			//$dsn = 'mysql:dbname='.$database.';host='.$hostname.';port='.$port;
+			//$this->db = new \PDO($dsn, $username, $password);
+			$this->db = new \PDO($dsn, $database_info['dbUsername'], $database_info['dbPassword']);
 		}
 		public static function getInstance(){
 			if(self::$instance == null){
@@ -22,4 +27,4 @@ class GetDb{
 		public function getDb(){
 			return $this->db;
 		}
-}	
+}
